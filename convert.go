92416ec6c8567188c5a4c01f10a1d8e6369e3642
zbh255 <565584326@qq.com>
2022-01-20 18:07:51 +0800

    在convert中预添加分割符，避免了在函数调用时的分配

--- convert.go
+++ convert.go
@@ -3,11 +3,12 @@ package bilog
 import "strconv"
 
 // 快速格式化年月日
+// year之外的数字如果小于10则会使用零填充prefix
 
 var yearBuf = func() []string {
 	strs := make([]string, 4096-1970)
 	for k := range strs {
-		strs[k] = strconv.Itoa(1970 + k)
+		strs[k] = strconv.Itoa(1970+k) + "-"
 	}
 	return strs
 }()
@@ -15,7 +16,10 @@ var yearBuf = func() []string {
 var monthBuf = func() []string {
 	strs := make([]string, 11)
 	for k := range strs {
-		strs[k] = strconv.Itoa(1 + k)
+		strs[k] = strconv.Itoa(1+k) + "-"
+		if 1+k < 10 {
+			strs[k] = "0" + strs[k]
+		}
 	}
 	return strs
 }()
@@ -23,7 +27,43 @@ var monthBuf = func() []string {
 var dayBuf = func() []string {
 	strs := make([]string, 31)
 	for k := range strs {
-		strs[k] = strconv.Itoa(1 + k)
+		strs[k] = strconv.Itoa(1+k) + " "
+		if 1+k < 10 {
+			strs[k] = "0" + strs[k]
+		}
+	}
+	return strs
+}()
+
+var hourBuf = func() []string {
+	strs := make([]string, 24)
+	for k := range strs {
+		strs[k] = strconv.Itoa(1+k) + ":"
+		if 1+k < 10 {
+			strs[k] = "0" + strs[k]
+		}
+	}
+	return strs
+}()
+
+var minuteBuf = func() []string {
+	strs := make([]string, 60)
+	for k := range strs {
+		strs[k] = strconv.Itoa(1+k) + ":"
+		if 1+k < 10 {
+			strs[k] = "0" + strs[k]
+		}
+	}
+	return strs
+}()
+
+var secondBuf = func() []string {
+	strs := make([]string, 60)
+	for k := range strs {
+		strs[k] = strconv.Itoa(1+k) + " "
+		if 1+k < 10 {
+			strs[k] = "0" + strs[k]
+		}
 	}
 	return strs
 }()
@@ -38,4 +78,17 @@ func fastConvertMonth(i int) string {
 
 func fastConvertDay(i int) string {
 	return dayBuf[i-1]
+}
+
+func fastConvertHour(i int) string {
+	return hourBuf[i-1]
+}
+
+//TODO: 修复分钟和秒的00产生的索引越界，0分时索引为-1
+func fastConvertMinute(i int) string {
+	return minuteBuf[i-1]
+}
+
+func fastConvertSecond(i int) string {
+	return secondBuf[i-1]
 }
