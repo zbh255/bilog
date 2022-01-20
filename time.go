638b245bbfed9a2fd3831320939fc13c1855bbbd
zbh255 <565584326@qq.com>
2022-01-20 18:06:03 +0800

    修改TimeFactory缓存时间的数据，减少了time.Now()和time.Year()...等方法的时间消耗，提高了2倍性能

--- time.go
+++ time.go
@@ -7,26 +7,55 @@ import (
 
 type TimeFactory struct {
 	rwMu sync.RWMutex
-	buf  *time.Time
+	buf  *date
+	raw  time.Time
+}
+
+type date struct {
+	Year   int
+	Month  int
+	Day    int
+	Hour   int
+	Minute int
+	Second int
 }
 
 func (t *TimeFactory) Start() {
 	if t.buf == nil {
-		tmp := time.Now()
-		t.buf = &tmp
+		t.raw = time.Now()
+		t.buf = &date{
+			Year:   t.raw.Year(),
+			Month:  int(t.raw.Month()),
+			Day:    t.raw.Day(),
+			Hour:   t.raw.Hour(),
+			Minute: t.raw.Minute(),
+			Second: t.raw.Second(),
+		}
 	}
 	go func() {
 		for {
-			time.Sleep(time.Millisecond)
+			time.Sleep(time.Second)
 			t.rwMu.Lock()
-			*t.buf = time.Now()
+			t.raw = time.Now()
+			t.buf.Year = t.raw.Year()
+			t.buf.Month = int(t.raw.Month())
+			t.buf.Day = t.raw.Day()
+			t.buf.Hour = t.raw.Hour()
+			t.buf.Minute = t.raw.Minute()
+			t.buf.Second = t.raw.Second()
 			t.rwMu.Unlock()
 		}
 	}()
 }
 
-func (t *TimeFactory) Get() time.Time {
+func (t *TimeFactory) Get() *date {
 	t.rwMu.RLock()
 	defer t.rwMu.RUnlock()
-	return *t.buf
+	return t.buf
+}
+
+func (t *TimeFactory) GetRaw() time.Time {
+	t.rwMu.RLock()
+	defer t.rwMu.RUnlock()
+	return t.raw
 }
