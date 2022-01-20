7c449e129507be9272ba697bce734dbe172cd480
zbh255 <565584326@qq.com>
2022-01-20 18:10:30 +0800

    修改Logger.SetPrefix的逻辑，消除了memove带来的开销，在频繁调用SetPrefix时提升了10%的性能。

--- log.go
+++ log.go
@@ -22,7 +22,6 @@ const (
 
 // 日志的级别
 
-
 // Logger atomic flag 会有公平性的问题
 type Logger struct {
 	mu      sync.Mutex
@@ -30,7 +29,7 @@ type Logger struct {
 	level   int
 	write   io.Writer
 	timeBuf []byte
-	prefix  []byte
+	prefix  string
 	buf     []byte
 }
 
@@ -41,27 +40,29 @@ func NewLogger(write io.Writer) *Logger {
 		level:   0,
 		write:   write,
 		buf:     make([]byte, BUFFER_SIZE),
-		prefix:  make([]byte, 0, PREFIX_BUF_SIZE),
 		timeBuf: make([]byte, 0, TIME_BUF_SIZE),
 		factory: factory,
 	}
 }
 
+// SetPrefix 复用string的内存来避免memove
 func (l *Logger) SetPrefix(s string) {
 	//for !atomic.CompareAndSwapInt32(l.flas, NoWriter,Writer) {}
 	l.mu.Lock()
 	defer l.mu.Unlock()
-	l.resetPrefixBuf()
-	l.prefix = append(l.prefix,s...)
+	l.prefix = s
 }
 
 func (l *Logger) fastConvert() {
 	l.resetTimeBuf()
-	now := l.factory.Get()
-	yy, mm, dd := now.Date()
-	l.timeBuf = append(l.timeBuf, fastConvertYear(yy)...)
-	l.timeBuf = append(l.timeBuf, fastConvertMonth(int(mm))...)
-	l.timeBuf = append(l.timeBuf, fastConvertDay(dd)...)
+	date := l.factory.Get()
+
+	l.timeBuf = append(l.timeBuf, fastConvertYear(date.Year)...)
+	l.timeBuf = append(l.timeBuf, fastConvertMonth(date.Month)...)
+	l.timeBuf = append(l.timeBuf, fastConvertDay(date.Day)...)
+	l.timeBuf = append(l.timeBuf, fastConvertHour(date.Hour)...)
+	l.timeBuf = append(l.timeBuf, fastConvertMinute(date.Minute)...)
+	l.timeBuf = append(l.timeBuf, fastConvertSecond(date.Second)...)
 }
 
 // 重置用于保存时间的缓冲区
@@ -78,13 +79,6 @@ func (l *Logger) resetWriteBuf() {
 	l.buf = *(*[]byte)(unsafe.Pointer(h))
 }
 
-// 重置前缀的缓冲区
-func (l *Logger) resetPrefixBuf()  {
-	h := (*reflect.SliceHeader)(unsafe.Pointer(&l.prefix))
-	h.Len = 0
-	l.prefix = *(*[]byte)(unsafe.Pointer(h))
-}
-
 func (l *Logger) Print(s string) {
 	//for !atomic.CompareAndSwapInt32(l.flas, NoWriter,Writer) {}
 	//defer func() {
@@ -96,8 +90,8 @@ func (l *Logger) Print(s string) {
 	l.resetWriteBuf()
 	// 获取日志的时间
 	l.fastConvert()
-	l.buf = append(l.buf, l.timeBuf...)
 	l.buf = append(l.buf, l.prefix...)
+	l.buf = append(l.buf, l.timeBuf...)
 	l.buf = append(l.buf, s...)
 
 	_, err := l.write.Write(l.buf)
