36f8aec2c45673ef4ce159644c84c6bca382e383
zbh255 <565584326@qq.com>
2022-01-21 12:02:27 +0800

    实现了Logger接口使日志更加易用，优化了SetPrefix的逻辑避免了其内部的memove。并实现了一个简单的输出缓冲区。

--- log.go
+++ log.go
@@ -15,45 +15,65 @@ import (
 */
 
 const (
-	BUFFER_SIZE     = 256
+	TOP_BUFFER_SIZE = 256
+	LOW_BUFFER_SIZE = TOP_BUFFER_SIZE * 6
 	TIME_BUF_SIZE   = 64
 	PREFIX_BUF_SIZE = 64
 )
 
-// 日志的级别
+// 缓存特殊的字符
+var (
+	cacheEntry byte = '\n'
+)
 
-// Logger atomic flag 会有公平性的问题
-type Logger struct {
-	mu      sync.Mutex
+// SimpleLogger atomic flag 会有公平性的问题
+type SimpleLogger struct {
+	mu sync.Mutex
+	// 缓存和生产date数据的工厂
 	factory *TimeFactory
-	level   int
-	write   io.Writer
+	// 设置的输出等级
+	level level
+	// 缓存level对应的string，避免频繁拷贝和分配
+	// 必须按照doc.go中定义的日志等级来初始化
+	levelCache []string
+	write      io.Writer
+	// 缓存格式化后的时间
 	timeBuf []byte
-	prefix  string
-	buf     []byte
+	//TODO:计划删除
+	//prefix string
+	// 顶层缓冲区
+	topBuf []byte
+	// 底层缓冲区
+	lowBuf []byte
 }
 
-func NewLogger(write io.Writer) *Logger {
+func NewLogger(write io.Writer, l level) Logger {
 	factory := &TimeFactory{}
 	factory.Start()
-	return &Logger{
-		level:   0,
-		write:   write,
-		buf:     make([]byte, BUFFER_SIZE),
+	return &SimpleLogger{
+		level: l,
+		write: write,
+		levelCache: []string{
+			"[INFO] ", "[DEBUG] ", "[TRACE] ", "[ERROR] ",
+			"[PANIC] ",
+		},
+		topBuf:  make([]byte, TOP_BUFFER_SIZE),
+		lowBuf:  make([]byte, 0, LOW_BUFFER_SIZE),
 		timeBuf: make([]byte, 0, TIME_BUF_SIZE),
 		factory: factory,
 	}
 }
 
 // SetPrefix 复用string的内存来避免memove
-func (l *Logger) SetPrefix(s string) {
-	//for !atomic.CompareAndSwapInt32(l.flas, NoWriter,Writer) {}
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.prefix = s
-}
+//func (l *SimpleLogger) SetPrefix(s string) {
+//	//for !atomic.CompareAndSwapInt32(l.flas, NoWriter,Writer) {}
+//	l.mu.Lock()
+//	defer l.mu.Unlock()
+//	l.prefix = s
+//}
 
-func (l *Logger) fastConvert() {
+// TODO优化转换速度
+func (l *SimpleLogger) fastConvert() {
 	l.resetTimeBuf()
 	date := l.factory.Get()
 
@@ -66,51 +86,133 @@ func (l *Logger) fastConvert() {
 }
 
 // 重置用于保存时间的缓冲区
-func (l *Logger) resetTimeBuf() {
+func (l *SimpleLogger) resetTimeBuf() {
 	h := (*reflect.SliceHeader)(unsafe.Pointer(&l.timeBuf))
 	h.Len = 0
 	l.timeBuf = *(*[]byte)(unsafe.Pointer(h))
 }
 
-// 重置用于输出的缓冲区
-func (l *Logger) resetWriteBuf() {
-	h := (*reflect.SliceHeader)(unsafe.Pointer(&l.buf))
-	h.Len = 0
-	l.buf = *(*[]byte)(unsafe.Pointer(h))
+// 写入到低层缓冲器，该函数会有一些检查
+func (l *SimpleLogger) writeLowBuf() {
+	if len(l.lowBuf)+len(l.topBuf) > cap(l.lowBuf) {
+		l.flushLowBuf()
+	}
+	l.lowBuf = append(l.lowBuf, l.topBuf...)
 }
 
-func (l *Logger) Print(s string) {
-	//for !atomic.CompareAndSwapInt32(l.flas, NoWriter,Writer) {}
-	//defer func() {
-	//	*l.flas = NoWriter
-	//}()
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	l.resetWriteBuf()
-	// 获取日志的时间
-	l.fastConvert()
-	l.buf = append(l.buf, l.prefix...)
-	l.buf = append(l.buf, l.timeBuf...)
-	l.buf = append(l.buf, s...)
-
-	_, err := l.write.Write(l.buf)
+// 将lowBuf中的数据全部写入到writer中并reset
+func (l *SimpleLogger) flushLowBuf() {
+	_, err := l.write.Write(l.lowBuf)
 	if err != nil {
 		panic(err)
 	}
-	//n := len(s) + len(l.prefix)
-	//sBytes := *(*[]byte)(unsafe.Pointer(&s))
-	//bufTmp := (*reflect.SliceHeader)(unsafe.Pointer(&l.buf))
-	//copy(l.buf, l.prefix)
-	//// 下一次拷贝从上次结束的位置开始
-	//bufTmp.Data = bufTmp.Data + uintptr(len(l.prefix))
-	//copy(*(*[]byte)(unsafe.Pointer(bufTmp)), sBytes)
-	//// 恢复指针
-	//bufTmp.Data = bufTmp.Data - uintptr(len(l.prefix))
-	//// 设置正确的长度并写入
-	//bufTmp.Len = n
-	//_, err := l.write.Write(*(*[]byte)(unsafe.Pointer(bufTmp)))
-	//if err != nil {
-	//	panic(err)
-	//}
+	l.resetLowBuf()
+}
+
+// 重置用于输出的缓冲区
+func (l *SimpleLogger) resetTopBuf() {
+	h := (*reflect.SliceHeader)(unsafe.Pointer(&l.topBuf))
+	h.Len = 0
+	l.topBuf = *(*[]byte)(unsafe.Pointer(h))
+}
+
+// 重置lowBuf
+func (l *SimpleLogger) resetLowBuf() {
+	h := (*reflect.SliceHeader)(unsafe.Pointer(&l.lowBuf))
+	h.Len = 0
+	l.lowBuf = *(*[]byte)(unsafe.Pointer(h))
+}
+
+func (l *SimpleLogger) Print(s string, level level) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.resetTopBuf()
+	// 获取日志的时间
+	l.fastConvert()
+	l.topBuf = append(l.topBuf, l.levelCache[level]...)
+	l.topBuf = append(l.topBuf, l.timeBuf...)
+	l.topBuf = append(l.topBuf, s...)
+
+	l.writeLowBuf()
+}
+
+func (l *SimpleLogger) Println(s string, level level) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.resetTopBuf()
+	// 获取日志的时间
+	l.fastConvert()
+	l.topBuf = append(l.topBuf, l.levelCache[level]...)
+	l.topBuf = append(l.topBuf, l.timeBuf...)
+	l.topBuf = append(l.topBuf, s...)
+	l.topBuf = append(l.topBuf, cacheEntry)
+
+	l.writeLowBuf()
+}
+
+// 验证输出的日志级别是否小于等于预设的级别
+func (l *SimpleLogger) checkLevel(level level) bool {
+	return level <= l.level
+}
+
+func (l *SimpleLogger) Level() int {
+	return int(l.level)
+}
+
+func (l *SimpleLogger) Info(s string) {
+	if !l.checkLevel(INFO) {
+		return
+	}
+	l.Println(s,INFO)
+}
+
+func (l *SimpleLogger) Debug(s string) {
+	if !l.checkLevel(DEBUG) {
+		return
+	}
+	l.Println(s,DEBUG)
+}
+
+func (l *SimpleLogger) Trace(s string) {
+	if !l.checkLevel(TRACE) {
+		return
+	}
+	l.Println(s,TRACE)
+}
+
+//TODO: 优雅地处理error
+func (l *SimpleLogger) ErrorFromErr(e error) {
+	if !l.checkLevel(ERROR) {
+		return
+	}
+	l.Println(e.Error(),ERROR)
+}
+
+func (l *SimpleLogger) ErrorFromString(s string) {
+	if !l.checkLevel(ERROR) {
+		return
+	}
+	l.Println(s,ERROR)
+}
+
+func (l *SimpleLogger) PanicFromErr(e error) {
+	if !l.checkLevel(PANIC) {
+		return
+	}
+	l.Println(e.Error(),ERROR)
+	panic(e)
+}
+
+func (l *SimpleLogger) PanicFromString(s string) {
+	if !l.checkLevel(PANIC) {
+		return
+	}
+	l.Println(s,ERROR)
+	panic(s)
+}
+
+func (l *SimpleLogger) Flush() {
+	l.flushLowBuf()
 }
