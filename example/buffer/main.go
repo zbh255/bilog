bf95e50205cd4c50dd6d3175453c1304e1a25a28
zbh255 <565584326@qq.com>
2022-03-17 17:26:41 +0800

    add example

---
+++ example/buffer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+	"github.com/zbh255/bilog"
+	"os"
+)
+
+func main() {
+	log := bilog.NewLogger(os.Stdout,bilog.PANIC,
+		bilog.WithTimes(),bilog.WithCaller(),bilog.WithTopBuffer(7),bilog.WithLowBuffer(2))
+	log.Info("buffer1")
+	log.Info("buffer 2")
+	log.Debug("buffer 3")
+	log.ErrorFromString("buffer 4")
+	log.ErrorFromErr(errors.New("My Error: buffer 5"))
+}
