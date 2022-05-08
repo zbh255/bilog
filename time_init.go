65f518c6f6697dbf798620d94048db99e73b693d
zbh255 <565584326@qq.com>
2022-05-08 18:25:35 +0800

    输出Goroutines检查Ci的错误

--- time_init.go
+++ time_init.go
@@ -1,7 +1,19 @@
 package bilog
 
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
 var timeFactory *TimeFactory
 
 func init() {
 	timeFactory = NewTimeFactory()
+	go func() {
+		for {
+			time.Sleep(time.Second / 10)
+			fmt.Println(runtime.NumGoroutine())
+		}
+	}()
 }
