49b40c96135612dadc5830bdac4a3fdcdb820965
zbh255 <565584326@qq.com>
2022-05-08 18:32:42 +0800

    尝试修复CI检测不了race的错误

--- time_init.go
+++ time_init.go
@@ -1,19 +1,7 @@
 package bilog
 
-import (
-	"fmt"
-	"runtime"
-	"time"
-)
-
 var timeFactory *TimeFactory
 
 func init() {
 	timeFactory = NewTimeFactory()
-	go func() {
-		for {
-			time.Sleep(time.Second / 10)
-			fmt.Println(runtime.NumGoroutine())
-		}
-	}()
 }
